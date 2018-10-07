7ecde2596e259606e8aa50dc4f070711d0cd835c
tylercarberry <tylercarberry@gmail.com>
2018-10-06 21:33:45 -0400

    Add initial spring server
    To test, use http://localhost:8080/rides/nearby?authToken=123

---
+++ server/src/main/java/com/rowan/ruber/RuberApplication.java
@@ -0,0 +1,12 @@
+package com.rowan.ruber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuberApplication.class, args);
+	}
+}
