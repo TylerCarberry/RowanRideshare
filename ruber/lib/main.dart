201578d84c8b5d1653ec9472544be327f5f5c4ac
TapanSoni <tapansoni97@gmail.com>
2018-10-08 17:09:13 -0400

    Added the second page - whenever you click a button, it goes to a sample second page

--- ruber/lib/main.dart
+++ ruber/lib/main.dart
@@ -1,73 +1,77 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    title: 'RUber',
+    home: MainScreen(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('RUber'), centerTitle: true,
-            ),
-            body: Center(
-                child: Column(
-                    children: [
-                      Container(
-                          margin: EdgeInsets.all(10.0),
-                          child: RaisedButton(
-                              child: Text('New Ride'),
-                              onPressed: () {
-
-                              }
-                          )
-                      ),
-                      Container(
-                          margin: EdgeInsets.all(10.0),
-                          child: RaisedButton(
-                            child: Text('Messages'),
-                            onPressed: () {
-
-                            },
-                          )
-                      ),
-                      Container(
-                          margin: EdgeInsets.all(10.0),
-                          child: RaisedButton(
-                            child: Text("Profile"),
-                            onPressed: () {
-
-                            },
-                          )
-                      ),
-                      Container(
-                          margin: EdgeInsets.all(10.0),
-                          child: RaisedButton(
-                            child: Text("Settings"),
-                            onPressed: () {
-
-                            },
-                          )
-                      )
-                    ]
-                )
-            ))
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('RUber'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              RaisedButton(
+                child: Text('New Ride'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => NewRideScreen()),
+                  );
+                },
+              ),
+              RaisedButton(
+                  child: Text('Messages'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NewRideScreen()),
+                    );
+                  }),
+              RaisedButton(
+                  child: Text('Profile'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NewRideScreen()),
+                    );
+                  }),
+              RaisedButton(
+                  child: Text('Settings'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NewRideScreen()),
+                    );
+                  }),
+            ],
+          ),
+        ));
   }
 }
 
-//class NewRideScreen extends StatelessWidget
-//{
-//  @override
-//  Widget build(BuildContext context)
-//  {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text("New Ride")
-//      ),
-//      body: Center(
-//        child: Column
-//      )
-//    );
-//  }
-//}
+class NewRideScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
