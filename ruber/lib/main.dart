131cdece33e4a990b51de1bc41aaf9104e96f709
TapanSoni <tapansoni97@gmail.com>
2018-10-08 19:27:56 -0400

    Added tab functionality with address field and a go button

--- ruber/lib/main.dart
+++ ruber/lib/main.dart
@@ -137,8 +137,26 @@ class NewRideScreen extends StatelessWidget {
           ),
           body: TabBarView(
             children: <Widget>[
-              Icon(Icons.directions_car, size: 50.0),
-              Icon(Icons.directions_run, size: 50.0),
+//              Icon(Icons.directions_car, size: 50.0),
+//              Icon(Icons.directions_run, size: 50.0),
+              Container(
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Starting Address: '
+                          )
+                      ),
+                      RaisedButton(
+                        child: Text('Go!'),
+                        onPressed: ()
+                        {
+
+                        },
+                      )
+                    ],
+                  )
+              )
             ],
           )
         )
