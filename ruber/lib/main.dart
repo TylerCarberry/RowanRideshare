22e8ddf84d1caaf6b4a3c5103e362c77e6ffc4e7
TapanSoni <tapansoni97@gmail.com>
2018-10-08 18:34:45 -0400

    Added a tabbed view to the second screen, and added icons + titles for the tabs. Also, fixed some bugs

--- ruber/lib/main.dart
+++ ruber/lib/main.dart
@@ -8,12 +8,16 @@ void main() {
 }
 
 class MainScreen extends StatelessWidget {
+  final String title;
+  MainScreen ({Key key, this.title}) : super (key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('RUber'),
           centerTitle: true,
+          automaticallyImplyLeading: false,
         ),
         body: Center(
           child: Column(
@@ -58,20 +62,87 @@ class MainScreen extends StatelessWidget {
 }
 
 class NewRideScreen extends StatelessWidget {
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Screen"),
+      appBar: AppBar(title: Text('New Ride'), centerTitle: true),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('RUber Menu'),
+              decoration: BoxDecoration(color: Colors.blue),
+            ),
+            ListTile(
+              leading: Icon(Icons.arrow_back),
+              title: Text('Main Menu'),
+              onTap: ()
+              {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+              onTap: ()
+              {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.account_box),
+              title: Text('Profile'),
+              onTap: ()
+              {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: ()
+              {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainScreen()),
+                );
+              },
+            ),
+          ],
+        )
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
-      ),
+      body: DefaultTabController(
+        length: 2,
+        child: new Scaffold(
+          appBar: AppBar(
+            actions: <Widget>[],
+            title: TabBar(
+              tabs: <Widget>[Tab(icon: Icon(Icons.directions_car), text: 'Going to Rowan'),
+                Tab(icon: Icon(Icons.directions_run), text: 'Going Home')
+              ],
+              indicatorColor: Colors.white,
+            ),
+            automaticallyImplyLeading: false,
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Icon(Icons.directions_car, size: 50.0),
+              Icon(Icons.directions_run, size: 50.0),
+            ],
+          )
+        )
+      )
     );
   }
 }
