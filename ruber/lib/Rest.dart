e520d8f94bf2d8629fa659c107df0bde6be8aab7
JohnStranahan <jasballpythons@msn.com>
2018-10-08 18:21:10 -0400

    Connected to spring server using local host

--- ruber/lib/Rest.dart
+++ ruber/lib/Rest.dart
@@ -8,11 +8,11 @@ class Rest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new Scaffold( body: new Container( //body of the scaffold is a container with a http pull
-          child: new FutureBuilder<List<User>>(
-            future: fetchUsersFromGitHub(),
+      home: new Scaffold(
+        body: new Container( //body of the scaffold is a container with a http pull
+          child: new FutureBuilder(
+            future: fetchResponse(),
             builder: (context, snapshot) {
-
               if (snapshot.hasData) {
                 return new ListView.builder(
                     itemCount: snapshot.data.length,
@@ -21,7 +21,8 @@ class Rest extends StatelessWidget {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
                             new Text(snapshot.data[index].name,
-                                style: new TextStyle(fontWeight: FontWeight.bold)),
+                                style: new TextStyle(
+                                    fontWeight: FontWeight.bold)),
                             new Divider()
                           ]
                       );
@@ -40,29 +41,15 @@ class Rest extends StatelessWidget {
     );
   }
 
-  Future<List<User>> fetchUsersFromGitHub() async {
-    final response = await http.get('https://api.github.com/users');
-    print(response.body);
-    List responseJson = json.decode(response.body.toString());
-    List<User> userList = createUserList(responseJson);
-    return userList;
+  Future fetchResponse() async {
+
+    List responseList;
+    http.Response response = await http.get("http://10.0.2.2:8080/hello/greeting").catchError((resp) {});
+    responseList = json.decode(response.body.toString());
+
+    return responseList;
+
+
   }
 
-  List<User> createUserList(List data){
-    List<User> list = new List();
-    for (int i = 0; i < data.length; i++) {
-      String title = data[i]["login"];
-      int id = data[i]["id"];
-      User user = new User(
-          name: title,id: id);
-      list.add(user);
-    }
-    return list;
-  }
-
-}
-class User {
-  String name;
-  int id;
-  User({this.name,this.id});
 }
