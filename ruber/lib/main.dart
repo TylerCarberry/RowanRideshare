90d635840cb26a79349f31644c3e1b6933f6d7b8
Michael Matthews <MM@Michaels-MacBook-Pro.local>
2018-10-19 09:02:45 -0400

    Seperated Ride Screen into seperate file

--- ruber/lib/main.dart
+++ ruber/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'Rest.dart';
+import 'login.dart';
+import 'RideScreen.dart';
+
+
 void main() {
   runApp(MaterialApp(
     title: 'RUber',
@@ -74,7 +78,7 @@ class TextForm extends StatefulWidget {
 }
 
 class _TextForm extends State<TextForm> {
-  final myController = TextEditingController();
+  final myController  = TextEditingController();
   final myController2 = TextEditingController();
   final myController3 = TextEditingController();
   final myController1 = TextEditingController();
@@ -87,195 +91,155 @@ class _TextForm extends State<TextForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: new Column(
-          children: <Widget>[
-            new ListTile(
-              leading: const Icon(Icons.person),
-              title: new TextField(
-                controller: myController,
-                decoration: new InputDecoration(
-                  hintText: "First Name",
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Login'),
+        ),
+        body: new Column(
+            children: <Widget>[
+              new ListTile(
+                leading: const Icon(Icons.person),
+                title: new TextField(
+                  controller: myController,
+                  decoration: new InputDecoration(
+                    hintText: "First Name",
+                  ),
+                  maxLength: 45,
                 ),
-                maxLength: 45,
               ),
-            ),
-            new ListTile(
-              leading: const Icon(Icons.person_outline),
-              title: new TextField(
-                controller: myController1,
-                decoration: new InputDecoration(
-                  hintText: "Last Name",
+              new ListTile(
+                leading: const Icon(Icons.person_outline),
+                title: new TextField(
+                  controller: myController1,
+                  decoration: new InputDecoration(
+                    hintText: "Last Name",
+                  ),
+                  maxLength: 45,
                 ),
-                maxLength: 45,
               ),
-            ),
-            new ListTile(
-              leading: const Icon(Icons.email),
-              title: new TextField(
-                controller: myController2,
-                decoration: new InputDecoration(
-                  hintText: "Email",
+              new ListTile(
+                leading: const Icon(Icons.email),
+                title: new TextField(
+                  controller: myController2,
+                  decoration: new InputDecoration(
+                    hintText: "Email",
+                  ),
+                  maxLength: 50,
                 ),
-                maxLength: 50,
               ),
-            ),
-            new ListTile(
-              leading: const Icon(Icons.location_city),
-              title: new TextField(
-                controller: myController3,
-                decoration: new InputDecoration(
-                  hintText: "Location",
+              new ListTile(
+                leading: const Icon(Icons.location_city),
+                title: new TextField(
+                  controller: myController3,
+                  decoration: new InputDecoration(
+                    hintText: "Location",
+                  ),
+                  maxLength: 50,
                 ),
-                maxLength: 50,
               ),
-            ),
-          ]
-      ) ,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          return showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                content: Text(myController.text + " " + myController1.text + " " + myController2.text + " " + myController3.text),
-              );
-            },
-          );
-        },
-        tooltip: 'Show me the value!',
-        child: Icon(Icons.text_fields),
-      ),
-    );
-  }
+            ]
+        ) ,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            return showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  content: Text(myController.text + " " + myController1.text + " " + myController2.text + " " + myController3.text),
+                );
+              },
+            );
+          },
+          tooltip: 'Show me the value!',
+          child: Icon(Icons.text_fields),
+        ),
+      );
+    }
 }
+
+//class MessagesScreen extends StatelessWidget {
+//
+//  @override
+//  Widget build(BuildContext context)
+//  {
+//    return Scaffold(
+//      appBar: AppBar(title: Text('New Ride'), centerTitle: true),
+//        child: Container(
+//          margin: const EdgeInsets.all(10.0),
+//          color: const Color(0xFF00FF00),
+//          width: 48.0,
+//          height: 48.0,
+//        ),
+//    );
+//  }
+//
+//}
 
 class NewRideScreen extends StatelessWidget {
 
   @override
-  Widget build(BuildContext context)
+  Widget build(context)
   {
-    return Scaffold(
-      appBar: AppBar(title: Text('New Ride'), centerTitle: true),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('RUber Menu'),
-              decoration: BoxDecoration(color: Colors.blue),
-            ),
-            ListTile(
-              leading: Icon(Icons.arrow_back),
-              title: Text('Main Menu'),
-              onTap: ()
-              {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MainScreen()),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.message),
-              title: Text('Messages'),
-              onTap: ()
-              {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MainScreen()),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.account_box),
-              title: Text('Profile'),
-              onTap: ()
-              {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MainScreen()),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: ()
-              {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MainScreen()),
-                );
-              },
-            ),
-          ],
-        )
-      ),
-      body: DefaultTabController(
-        length: 2,
-        child: new Scaffold(
-          appBar: AppBar(
-            actions: <Widget>[],
-            title: TabBar(
-              tabs: <Widget>[Tab(icon: Icon(Icons.directions_car), text: 'Going to Rowan'),
-                Tab(icon: Icon(Icons.directions_run), text: 'Going Home')
-              ],
-              indicatorColor: Colors.white,
-            ),
-            automaticallyImplyLeading: false,
-          ),
-          body: TabBarView(
-            children: <Widget>[
-//              Icon(Icons.directions_car, size: 50.0),
-//              Icon(Icons.directions_run, size: 50.0),
-              Container(
-                  child: Column(
-                    children: <Widget>[
-                      TextFormField(
-                          decoration: InputDecoration(
-                              labelText: 'Starting Address: '
-                          )
-                      ),
-                      RaisedButton(
-                        child: Text('Go!'),
-                        onPressed: ()
-                        {
-
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => Rest()),
-                          );
-
-                        },
-                      )
-                    ],
-                  )
-              ),
-              Container(
-                  child: Column(
-                    children: <Widget>[
-                      TextFormField(
-                          decoration: InputDecoration(
-                              labelText: 'Destination Address: '
-                          )
-                      ),
-                      RaisedButton(
-                        child: Text('Go!'),
-                        onPressed: ()
-                        {
-
-                        },
-                      )
-                    ],
-                  )
+      return Scaffold(
+          appBar: AppBar(title: Text('New Ride'), centerTitle: true),
+          drawer: Drawer(
+              child: ListView(
+                children: <Widget>[
+                  DrawerHeader(
+                    child: Text('RUber Menu'),
+                    decoration: BoxDecoration(color: Colors.blue),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.arrow_back),
+                    title: Text('Main Menu'),
+                    onTap: ()
+                    {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MainScreen()),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.message),
+                    title: Text('Messages'),
+                    onTap: ()
+                    {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MainScreen()),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.account_box),
+                    title: Text('Profile'),
+                    onTap: ()
+                    {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MainScreen()),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text('Settings'),
+                    onTap: ()
+                    {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MainScreen()),
+                      );
+                    },
+                  ),
+                ],
               )
-            ],
+          ),
+          body: DefaultTabController(
+              length: 2,
+              child: launchRideScreen(context) //Launches method in RideScreen.dart file
           )
-        )
-      )
-    );
+      );
   }
 }
