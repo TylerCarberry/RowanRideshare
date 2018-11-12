80f40d8fc7f122c89a66790d3a0ca873b2b962d3
tylercarberry <tylercarberry@gmail.com>
2018-11-12 14:53:06 -0500

    Convert string address to coordinates

---
+++ server/src/main/java/com/rowan/ruber/model/google_maps/Viewport.java
@@ -0,0 +1,25 @@
+
+package com.rowan.ruber.model.google_maps;
+
+public class Viewport {
+
+    private Northeast northeast;
+    private Southwest southwest;
+
+    public Northeast getNortheast() {
+        return northeast;
+    }
+
+    public void setNortheast(Northeast northeast) {
+        this.northeast = northeast;
+    }
+
+    public Southwest getSouthwest() {
+        return southwest;
+    }
+
+    public void setSouthwest(Southwest southwest) {
+        this.southwest = southwest;
+    }
+
+}
