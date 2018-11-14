909a379795c62568ccf567c4f7c32152995545b0
Nic <ncklasala@gmail.com>
2018-11-14 17:45:52 -0500

    potential newuser on first auth.

--- ruber/lib/AuthScreen.dart
+++ ruber/lib/AuthScreen.dart
@@ -1,10 +1,10 @@
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:io';
-
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -75,6 +75,11 @@ class _MyHomePageState extends State<MyHomePage> {
     assert(user.displayName != null);
     assert(!user.isAnonymous);
     assert(await user.getIdToken() != null);
+    var now = new DateTime.now();
+    if(user.getIdToken() == null){
+      NewUser newUser = new NewUser(user.uid, user.displayName,user.email,now);
+      createPost(newUser);
+    }
 
     final FirebaseUser currentUser = await _auth.currentUser();
     assert(user.uid == currentUser.uid);
@@ -163,4 +168,55 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+}
+
+class NewUser{
+  String id;
+  String name;
+  String email;
+  var createdDate;
+  var schedules;
+//  var schedules;
+  NewUser(String id,String name, String email, var createdDate){
+    this.id = id;
+    this.name =name;
+    this.email =email;
+    this.createdDate = createdDate;
+    this.schedules;
+  }
+
+//  factory NewUser.fromJson(Map<String, dynamic> parsedJson) {
+//    return NewUser(
+//      id: parsedJson["id"],
+//      name: parsedJson["name"],
+//      email: parsedJson["email"],
+//      createdDate: parsedJson["createdDate"],
+//
+//    );}
+
+  Map<String, dynamic> toJson() =>
+      {
+        "id": id,
+        "name": name,
+        "email": email,
+        "createdDate": createdDate,
+        "schedules":schedules,
+      };
+
+
+}
+
+String postToJson(NewUser data) {
+  final dyn = data.toJson();
+  return json.encode(dyn);
+}
+String url = 'http://10.0.2.2:8080/rides/profile/69';
+Future<http.Response> createPost(NewUser post) async {
+  final response = await http.post('$url',
+      headers: {
+        HttpHeaders.contentTypeHeader: 'application/json',
+        HttpHeaders.authorizationHeader: ''
+      },
+      body: postToJson(post));
+  return response;
 }
