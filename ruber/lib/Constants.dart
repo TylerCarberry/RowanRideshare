e0d301e0a635978dc65529e2ebdd0a61753098bc
tylercarberry <tylercarberry@gmail.com>
2018-12-02 15:00:39 -0500

    Use local server by default

--- ruber/lib/Constants.dart
+++ ruber/lib/Constants.dart
@@ -1,4 +1,4 @@
-final String BASE_URL = "http://ec2-34-230-33-210.compute-1.amazonaws.com:8080";
+// If you want to use your AWS instead of your local server, uncomment this line
+// final String BASE_URL = "http://ec2-34-230-33-210.compute-1.amazonaws.com:8080";
 
-// If you want to use your local server instead of AWS, uncomment this line
-// final String BASE_URL = "http://10.0.2.2:8080";
+final String BASE_URL = "http://10.0.2.2:8080";
